066553fd773fadc983a617d785adab0fa7810e69
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-20 14:05:51 +0200

    visualize 10 email adresses
    With an external for cicle we generate 10 differents random mails,
    created an array,
    for all the intercaction in the cycle, push the mail in the array,
    created a ul in html and with a vfor visualize all the mails

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,19 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-
+         mailList: [],
       }
     },
     methods: {
       
+    },
+    mounted() {
+      for (let i = 0; i < 10; i++) {
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then(response => {         
+            this.mailList.push(response.data.response);            
+         })
+         
+      }
     },
   }).mount('#app')
